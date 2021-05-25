6ecb9e816c3852af969ea6cbc62a07c81aa13bce
abildgaard06 <abildgaard06@hotmail.com>
2021-05-25 20:02:38 +0200

    CSS

--- template/header.php
+++ template/header.php
@@ -3,16 +3,42 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Homepage</title>
+        <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
     <div id="header">
-    <link rel="stylesheet" href="css/style.css">
+      <h6>header</h6>
+      <img src="pokemon.jpg" align="left">
     <header>
-      <div id="navbar">
-      <nav class="topnav">
-        <div class="leftnav">
-          <ul>
+      </div>
+    <div id="navbar">
+      <h6>navbar</h6>
+    </div>
+      <div id="row">
+        <h6>row</h6>
+      <div id="side">
+        <h6>Side</h6>
+  </div>
+      <div id="main">
+        <h6>main</h6>
+  </div>
+  <div id="maintext">
+    <h6>maintext</h6>
+  </div>
+      </div>
+      <div id="footer">
+        <h6>footer</h6>
+      <div id="left">
+        <h6>left</h6>
+  </div>
+    <div id="middle">
+      <h6>middle</h6>
+  </div>
+  <div id="right">
+    <h6>middle</h6>
+  </div>
+  </div>
             <?php foreach(getNav() as $nav) { ?>
             <li>
               <a href="?p=<?php echo $nav['id']; ?>">
@@ -20,7 +46,4 @@
               </a>
             </li>
           <?php } ?>
-          </ul>
-        </div>
-      </nav>
-    </header>
+            </header>
