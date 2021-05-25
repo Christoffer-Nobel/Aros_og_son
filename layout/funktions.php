fbb2a3c5fe4531ba56ba3c14811a01ff2e862ddd
kajkage <73479384+kajkage@users.noreply.github.com>
2021-05-25 11:56:17 +0200

    code
    indsat kode til hjemmesiden

--- layout/funktions.php
+++ layout/funktions.php
@@ -17,5 +17,47 @@ function connect(){
   }
 
   //vælge den database vi gerne vil bruge
-  mysqli_select_db($conn, DBNAME);
+  mysqli_selec// vi laver ny funktion bed navn getNav
+  function getNav() {
+    //sætter variablen "conn" til at være global (den åbner $conn så den ikke kun funggere i funktionen, men kan hente data udefra)
+    global $conn;
+  // tager titler og id'er fra alle vores sider og lægger i sql
+    $sql = 'SELECT id, title FROM pages';
+  // sender en forspørgsel efter data til dabasen og samler det i result
+    $result = mysqli_query($conn, $sql);
+    // opretter et tomt array
+    $nav = [];
+  //hvis antallet af rows i results er større end 0 eksekvere den while loopet
+    if(mysqli_num_rows($result) > 0) {
+      //
+      while($row = mysqli_fetch_assoc($result)) {
+        //sætter resultatet ind i det tomme array og samtidig sætter ind som kun id og titel
+        $nav[] =$row;
+      }
+    }
+    return $nav;
+    // retunere resultatet
+  }
+t_db($conn, DBNAME);
+}
+// vi laver ny funktion bed navn getNav
+function getNav() {
+  //sætter variablen "conn" til at være global (den åbner $conn så den ikke kun funggere i funktionen, men kan hente data udefra)
+  global $conn;
+// tager titler og id'er fra alle vores sider og lægger i sql
+  $sql = 'SELECT id, title FROM pages';
+// sender en forspørgsel efter data til dabasen og samler det i result
+  $result = mysqli_query($conn, $sql);
+  // opretter et tomt array
+  $nav = [];
+//hvis antallet af rows i results er større end 0 eksekvere den while loopet
+  if(mysqli_num_rows($result) > 0) {
+    //
+    while($row = mysqli_fetch_assoc($result)) {
+      //sætter resultatet ind i det tomme array og samtidig sætter ind som kun id og titel
+      $nav[] =$row;
+    }
+  }
+  return $nav;
+  // retunere resultatet
 }
