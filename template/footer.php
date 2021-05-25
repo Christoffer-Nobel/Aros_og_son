6ca9d33ac9c1b687bd5a4f2140beac972bde0b80
abildgaard06 <abildgaard06@hotmail.com>
2021-05-25 22:29:49 +0200

    CSS på en helt ny måde

--- template/footer.php
+++ template/footer.php
@@ -1,4 +1,19 @@
 
-
+</div>
+</div>
+<div id="footer">
+  <div id="left">
+      <h6><a href="https://www.google.com/maps/place/Seebladsgade+1,+5000+Odense/@55.4035443,10.3776725,17z/data=!4m5!3m4!1s0x464cdffcfcc4e6a1:0xd5ca23165eb2230f!8m2!3d55.4035443!4d10.3798612">Aros & Søn ApS</a></h6>
+      <h6>Seebladsgade 1</h6>
+      <h6>5000 Odense C</h6></div>
+      <div id="middle"><h6>Kontakt</h6>
+      <h6><a href="tel:004528566267">+4528566267</a></h6>
+      <h6><a href="mailto:cema@ucl.com">kontakt@arosogsøn.dk</a></h6></div>
+    <div id="right">
+      <h6>Aros & Søn ApS er et voksende revisionfirma</h6>
+      <h6>med over 3700 kunder og flere nye på vej.</h6>
+      <h6>Vi vil være Fyns førende revisionsfirma.</h6></div>
+</div>
+    </header>
 </body>
 </html>
--- template/header.php
+++ template/header.php
@@ -5,45 +5,21 @@
     <meta charset="utf-8">
     <title>Homepage</title>
         <link rel="stylesheet" href="css/style.css">
+    <img id="image" src="css/logo.png" align="left">
   </head>
   <body>
     <div id="header">
-      <h6>header</h6>
-      <img src="css/pictures/tihi.jpg" align="left">
+      <h4>Aros & Søn ApS</h4>
     <header>
       </div>
-    <div id="navbar">
-      <h6>navbar</h6>
-    </div>
-      <div id="row">
-        <h6>row</h6>
+      <div id="page">
+      <div id="navbar">
+                  <?php foreach(getNav() as $nav) { ?>
+                  <li>
+                    <a href="?p=<?php echo $nav['id']; ?>">
+                      <?php echo $nav['title']; ?>
+                    </a>
+                  </li>
+                <?php } ?>
+              </div>
       <div id="side">
-        <h6>Side</h6>
-  </div>
-      <div id="main">
-        <h6>main</h6>
-  </div>
-  <div id="maintext">
-    <h6>maintext</h6>
-  </div>
-      </div>
-      <div id="footer">
-        <h6>footer</h6>
-      <div id="left">
-        <h6>left</h6>
-  </div>
-    <div id="middle">
-      <h6>middle</h6>
-  </div>
-  <div id="right">
-    <h6>middle</h6>
-  </div>
-  </div>
-            <?php foreach(getNav() as $nav) { ?>
-            <li>
-              <a href="?p=<?php echo $nav['id']; ?>">
-                <?php echo $nav['title']; ?>
-              </a>
-            </li>
-          <?php } ?>
-            </header>
