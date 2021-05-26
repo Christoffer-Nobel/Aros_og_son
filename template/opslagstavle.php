7833d0e6da49b8b14edd55ff37b7b8d4b6120b6b
kvn27401 <73479557+kvn27401@users.noreply.github.com>
2021-05-26 15:45:33 +0200

    bulletboard virker næsten

--- template/opslagstavle.php
+++ template/opslagstavle.php
@@ -1,6 +1,51 @@
 <?php include('header.php'); ?>
-<!-- Vi inkludere header.php, efterfølgende echo via php titlen fra page, så sætter vi et billede ind og til sidst echo vi vore body tekst som vi har lavet i phpmyadmin -->
   <h2><?php echo $page['title']; ?><h2>
+<?php
+global $conn;
+?>
+<h2> Skriv et opslag: </h2>
+<form  method="post">
+<input type="number" name="empid" placeholder="Medarbjeder nr" required> <br>
+<input type="text" name="title" placeholder="Titel" required><br>
+<input type="text" name="posttext" placeholder="Tekst" required>
+<input type="submit" name="makepost" value="Del"><br> <br>
+</form>
 
+<?php
+$sql = "SELECT * from employeeposts where employee_id > 0";
+
+$result = mysqli_query($conn, $sql);
+$posts = [];
+if(mysqli_num_rows($result) > 0){
+  while($row = mysqli_fetch_assoc($result)){
+    $posts[] = $row;
+  }
+}
+foreach ($posts as $post) {
+  echo $post['e_firstname'] . " " . $post['e_lastname']; ?> <br> <?php
+  echo $post['title']; ?> <br> <?php
+  echo $post['bulletboard_text']; ?> <br> <?php
+  echo $post['created_at']; ?> <br> <?php
+?> <br> <br> <?php
+}
+
+if(isset($_POST['makepost'])){
+  $empid = $_POST['empid'];
+  $title = $_POST['title'];
+  $bulletboard_text = $_POST['posttext'];
+
+  $sql = "INSERT INTO note (employee_id, title, bulletboard_text) VALUES ('$empid', '$title', '$bulletboard_text');";
+  $result = mysqli_query($conn, $sql);
+  if (false===$result) {
+    printf(mysqli_error($conn));
+  }
+  header("Location: ./index.php?p=1");
+
+}
+?>
+
+
+</body>
+</html>
 
 <?php include('footer.php'); ?>
