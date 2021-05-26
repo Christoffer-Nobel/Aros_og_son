922b7bd44de6dfdff8ff80942d5753423673b95b
Christoffer-Nobel <christoffernobel1708@gmail.com>
2021-05-26 15:12:23 +0200

    flere ting

--- template/singleCus.php
+++ template/singleCus.php
@@ -9,7 +9,7 @@ $cus = [];
 if(mysqli_num_rows($result) > 0){
   while($row = mysqli_fetch_assoc($result)){
     $cus[] = $row;
-}
+  }
 }
 
 
@@ -17,26 +17,27 @@ if(mysqli_num_rows($result) > 0){
 
 <body>
   <h1><?php echo "Navn: " . $cus[0]["firstname"] . " " . $cus[0]["lastname"]; ?></h1>
-  <h2> <?php echo "Kunde nr: 0000" . $cus[0]["customer_id"];?>
+    <h2> <?php echo "Kunde nr: 0000" . $cus[0]["customer_id"];?>
   <br>
-  <h2> <?php echo "Firma: " . $cus[0]["company_name"]; ?>
+    <h2> <?php echo "Firma: " . $cus[0]["company_name"]; ?>
   <br>
-  <h2> <?php echo "Email: " . $cus[0]["email"]; ?>
+    <h2> <?php echo "Email: " . $cus[0]["email"]; ?>
   <br>
-  <h2> <?php echo "Tlf: " . $cus[0]["phone_number"]; ?>
-<br><br>
+    <h2> <?php echo "Tlf: " . $cus[0]["phone_number"]; ?>
+  <br><br>
 <?php echo "Rådgivere:" ?> <h3>
   <?php foreach($cus as $emp) {
     echo $emp["e_firstname"];
-    ?> <br> <?php
+      ?> <br> <?php
   }
-  ?> </h3>
+?> </h3>
 
-<h2> Noter til kunde: </h2>
-<form method="post">
-<input type="textarea" name="notetext">
-<input type="submit" value="Tilføj note">
-</form>
+  <h2> Noter til kunde: </h2>
+    <form method="post">
+      <input type="textarea" name="notetext">
+      <input type="submit" value="Tilføj note">
+    </form>
+
 <?php
 
 $sql = "SELECT * from customernotes where customer_id = '$pid'";
@@ -48,12 +49,12 @@ if(mysqli_num_rows($result) > 0){
     $notes[] = $row;
   }
 
-foreach ($notes as $singleNote) {
-  echo $singleNote["created_at"];
-  ?><br> <?php
-  echo $singleNote["note_text"];
-    ?><br> <?php  ?><br> <?php
-}
+  foreach ($notes as $singleNote) {
+    echo $singleNote["created_at"];
+      ?><br> <?php
+    echo $singleNote["note_text"];
+      ?><br> <?php  ?><br> <?php
+  }
 }
 if(isset($_POST['notetext'])){
   $note = $_POST['notetext'];
