0ae819e0fe200da7e8ea57476b41a8aabc40c59a
Christoffer-Nobel <christoffernobel1708@gmail.com>
2021-05-26 20:18:59 +0200

    login start

--- template/login.php
+++ template/login.php
@@ -17,6 +17,35 @@
         <input type="submit" name="" value="Login" href="../template/index.php">
       <a id="nybruger" href="./customers.php">Ny bruger</a>
 
+<?php
+      if(isset($_POST['btnlogin']))
+      {
+      //sætter den indskrevne email og password i en variabel
+          $uname = $_POST['email'];
+          $pass = $_POST['password'];
+          $password = hash('ripemd160', $pass);
+          $users = getUser($uname);
+
+
+            for($i = 0; $i < count($users); $i++){
+      //tjekker om det indskrevne email og password med vores hash passer med samme data fra hver bruger i vores array indtil den finder et match, og sætter her en variabel til velkommen, og en variabel til brugerens navn
+              if($users[$i]['email'] == $uname && $users[$i]['password'] == $password)
+              {
+                  $msg = "<p>Velkommen</p>";
+                  $name = $users[$i]["name"];
+
+                  break;
+              }
+      //Hvis den ikke finder et match sættes en variabel, til "forkert" og variblen med navn til ingenting
+              else
+              {
+                 $msg = "<p>Forkert brugernavn eller adgangskode</p>";
+                 $name = null;
+              }
+          }
+          echo $msg . $name;
+        }
+?>
 
   </body>
 </html>
