29409d2fc0e90b75313e0f20f616ea2212dd7bef
kvn27401 <73479557+kvn27401@users.noreply.github.com>
2021-05-27 14:48:15 +0200

    ændring i link til navn istedet for vis mere

--- template/customers.php
+++ template/customers.php
@@ -15,8 +15,8 @@ $result = mysqli_query($conn, $sql);
 
 echo "<table>"; // start a table tag in the HTML
 while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results?>
-<tr><td><a href="?t=<?php echo $row["customer_id"]; ?>">Vis mere</a> <?php
-echo "<td>" . $row['firstname'] . " " . $row['lastname'] . "</td><td>" . $row['company_name'] . "</td><td>" . $row['email'] . "</td><td>" . $row['phone_number'] . "</td></tr>";  //$row['index'] the index here is a field name
+<tr><td><a href="?t=<?php echo $row["customer_id"]; ?>"><?php echo $row['firstname'] . " " . $row['lastname']?></a><?php
+echo "</td><td>" . $row['company_name'] . "</td><td>" . $row['email'] . "</td><td>" . $row['phone_number'] . "</td></tr>";  //$row['index'] the index here is a field name
 }
 
 echo "</table>"; //Close the table in HTML
